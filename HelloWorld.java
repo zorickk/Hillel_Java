830508c978b4c79562cf1957fe263647c21727c9
Vitalii Ivanov <zorickk@gmail.com>
2020-06-22 00:10:41 +0300

    Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+	public static void main(String [] args) {
+		
+		String name = "Hello";
+		String names = "World!!!";
+		System.out.println("--------------------------");
+		System.out.println("              ");
+		System.out.println(" .--I\"\"\"\"\"\"\"\"\"\"I");
+		System.out.println("/ - I  KAMAZ   I");
+		System.out.println("''-O-\"\"\"\"\"\"O\"O\"\"-");
+		System.out.println("              ");
+		System.out.println(name + " " + names);
+		System.out.println("              ");
+		System.out.println("--------------------------");
+	}
+}
