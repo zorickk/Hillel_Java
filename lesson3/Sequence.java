a62c3268e6724e4055addab7e2076c63032747c7
Vitalii Ivanov <zorickk@gmail.com>
2020-07-01 23:35:13 +0300

    update

---
+++ lesson3/Sequence.java
@@ -0,0 +1,12 @@
+package lesson3;
+
+public class Sequence {
+    public static void main(String[] args) {
+
+        for (int i = 0; i > -50; i--) {
+            if (i % 5 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
